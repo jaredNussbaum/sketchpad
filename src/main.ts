d4038e5c509eb16003641d10b523743a0ea3e00b
jaredNussbaum <74433775+jaredNussbaum@users.noreply.github.com>
2025-10-30 07:08:04 +0000

    stack functionality (step 4)

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,14 @@ render.strokeStyle = "black";
 document.body.appendChild(canvas);
 
 let drawing = false;
-let lastX = 0;
-let lastY = 0;
+let last_x = 0;
+let last_y = 0;
 
-canvas.addEventListener("mousedown", (e) => {
+/*canvas.addEventListener("mousedown", (e) => {
   drawing = true;
   const rect = canvas.getBoundingClientRect();
-  lastX = e.clientX - rect.left;
-  lastY = e.clientY - rect.top;
+  last_x = e.clientX - rect.left;
+  last_y = e.clientY - rect.top;
 });
 
 canvas.addEventListener("mousemove", (e) => {
@@ -32,35 +32,95 @@ canvas.addEventListener("mousemove", (e) => {
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
   render.beginPath();
-  render.moveTo(lastX, lastY);
+  render.moveTo(last_x, last_y);
   render.lineTo(x, y);
   render.stroke();
-  lastX = x;
-  lastY = y;
+  last_x = x;
+  last_y = y;
 });
 
 ["mouseup", "mouseleave"].forEach((eventType) => {
   canvas.addEventListener(eventType, () => (drawing = false));
 });
-
+*/
 const clear = document.createElement("button");
 clear.textContent = "Clear";
 clear.addEventListener("click", () => {
+  last_stroke = [];
+  sketch = [];
   render.clearRect(0, 0, canvas.width, canvas.height);
 });
 document.body.appendChild(clear);
 
-const undo = document.createElement("button");
-undo.textContent = "Undo";
-undo.addEventListener("click", () => {
-  //undo
+type Point = { x: number; y: number };
+let sketch: Point[][] = [];
+let last_stroke: Point[] = [];
+
+const DRAWING_CHANGED = "DRAWING_CHANGED";
+
+canvas.addEventListener(DRAWING_CHANGED, () => {
+  render.clearRect(0, 0, 256, 256);
+  render.beginPath();
+  for (const line of sketch) {
+    if (line.length < 2) {
+      continue;
+    }
+    render.moveTo(line[0].x, line[0].y);
+    for (let i = 0; i < line.length; i++) {
+      render.lineTo(line[i].x, line[i].y);
+    }
+    render.stroke();
+  }
 });
-document.body.appendChild(clear);
+
+canvas.addEventListener("mousedown", (pos) => {
+  const canvas_rect = canvas.getBoundingClientRect();
+  last_stroke = [{
+    x: pos.clientX - canvas_rect.left,
+    y: pos.clientY - canvas_rect.top,
+  }];
+  sketch.push(last_stroke);
+  drawing = true;
+});
+
+canvas.addEventListener("mousemove", (pos) => {
+  if (!drawing) return;
+  const canvas_rect = canvas.getBoundingClientRect();
+  const x = pos.clientX - canvas_rect.left;
+  const y = pos.clientY - canvas_rect.top;
+  last_stroke.push({ x, y });
+  canvas.dispatchEvent(new Event(DRAWING_CHANGED));
+});
+
+["mouseup", "mouseleave"].forEach((event) =>
+  canvas.addEventListener(event, () => (drawing = false))
+);
+
+let stack: Point[][] = [];
 
 const redo = document.createElement("button");
 redo.textContent = "Redo";
 redo.addEventListener("click", () => {
   //redo
+  /*if (sketch.length == 0) return;*/
+  const target = stack.pop()!;
+  if (target) {
+    sketch.push(target);
+  }
+  canvas.dispatchEvent(new Event(DRAWING_CHANGED));
+});
+
+const undo = document.createElement("button");
+undo.textContent = "Undo";
+undo.addEventListener("click", () => {
+  //undo
+  if (sketch.length == 0) return;
+  const target = sketch.pop()!;
+  if (target) {
+    stack.push(target);
+  }
+
+  canvas.dispatchEvent(new Event(DRAWING_CHANGED));
 });
 
 document.body.appendChild(clear);
